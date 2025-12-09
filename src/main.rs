757246e7574bfd08d947050834f53141e5c9460d
Kiiyo <google.outshine921@passinbox.com>
2025-12-09 08:10:16 +0800

    to container

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,6 @@ use bot::main::run_bot;
 async fn main() {
     dotenv().ok();
 
-    let bot_token = env::var("DISCORD_TEST_TOKEN").expect("token not set");
+    let bot_token = env::var("DISCORD_TOKEN").expect("token not set");
     run_bot(&bot_token).await;
 }
