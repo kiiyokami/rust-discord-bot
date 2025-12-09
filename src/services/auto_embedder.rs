9802336087cab41e649cbccb63b2cdaba2017e6b
kiiyo <85468568+kiiyokami@users.noreply.github.com>
2025-12-10 07:41:18 +0800

    fix embed

--- src/services/auto_embedder.rs
+++ src/services/auto_embedder.rs
@@ -14,13 +14,13 @@ pub fn get_domain_map() -> HashMap<&'static str, &'static str> {
 }
 
 pub fn auto_embed(link: &str) -> Option<String> {
-    let mut url_str = link.replace("www.", "");
+    let url_str = link.replace("www.", "");
     let domain_map = get_domain_map();
+    
     for (domain, embed_domain) in domain_map.iter() {
-        println!("Checking domain: {}", domain);
-        if url_str.contains(domain) {
-            url_str = url_str.replace(domain, embed_domain);
-            return Some(url_str);
+        let pattern = format!("://{}", domain);
+        if url_str.contains(&pattern) {
+            return Some(url_str.replace(domain, embed_domain));
         }
     }
     None
